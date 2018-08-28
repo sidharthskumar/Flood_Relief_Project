73797b2d87e8d935d9dce0c5be42fb702971c703
abin <abinshoby@gmail.com>
2018-08-28 18:10:16 +0530

    search function changed from contains to match

--- search.py
+++ search.py
@@ -1,7 +1,7 @@
 import pandas as pd
 def search(name):
     data = pd.read_csv('../data/details.csv', sep='\t')
-    l=data[data['name'].str.contains(name)]
+    l=data[data['name'].str.match(name)]
     print(l)
     #return (l['name'].values.tolist()[0],l['address'].values.tolist()[0],l['camp'].values.tolist()[0])
     return l
